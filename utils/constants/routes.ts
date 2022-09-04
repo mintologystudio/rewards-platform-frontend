ffee7275c6b95c4f19edd12d9d2e247021dc7d4b
Eric Mong <m.changhsi@gmail.com>
2022-09-04 18:51:54 +0800

    Update routes mapping

--- utils/constants/routes.ts
+++ utils/constants/routes.ts
@@ -1,8 +1,12 @@
 const Routes = {
+  HOME: '/',
   VIEW_CAMPAIGN: '/campaign',
   CREATE_CAMPAIGN: '/merchant/campaign/create',
   EDIT_CAMPAIGN: '/merchant/campaign/edit',
   ERROR: '/error',
+  LOGIN: '/login',
+  EXPLORE: '/explore',
+  PROFILE: '/profile',
 }
 
 export default Routes
