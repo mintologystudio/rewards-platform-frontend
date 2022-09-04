ede7c61b3618a6c1f0d6d87ff20789e9b67745a9
Eric Mong <m.changhsi@gmail.com>
2022-09-04 18:55:39 +0800

    Refactor external api service

--- utils/api.ts
+++ utils/api.ts
@@ -1,59 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
-import { IAssetData, IGasPrices, IOwnedNFTData, IParsedTx } from './interfaces'
+import { IAssetData, IGasPrices, IParsedTx } from './interfaces'
 import envConfig from './envConfig'
-
-let apiUrl = envConfig.API_URL
-let moralisAPIURL = 'https://deep-index.moralis.io/api/v2'
-
-let config = {
-  headers: {
-    'X-API-Key': envConfig.MORALIS_API_KEY,
-  },
-}
-
-const moralisApi: AxiosInstance = axios.create({
-  baseURL: moralisAPIURL,
-  timeout: 30000, // 30 secs
-  headers: {
-    'X-API-Key': envConfig.MORALIS_API_KEY,
-  },
-})
-
-export const getTokensFromUser = async (
-  userAddr: string,
-  collectionAddr: string
-): Promise<IOwnedNFTData[]> => {
-  try {
-    const response: any = await moralisApi.get(`/${userAddr}/nft/${collectionAddr}?chain=eth&format=decimal`)
-
-    let tokens: IOwnedNFTData[] = response.data.result.map(async (res: any) => {
-      try {
-        if (!res.token_uri) {
-          return {
-            tokenId: 0,
-            tokenImage: '',
-          }
-        }
-
-        const _response = await axios.get(res.token_uri)
-
-        return {
-          tokenId: Number(res.token_id),
-          tokenImage: _response.data.image,
-        }
-      } catch (error) {
-        return {
-          tokenId: 0,
-          tokenImage: '',
-        }
-      }
-    })
-
-    return await Promise.all(tokens)
-  } catch (error) {
-    return []
-  }
-}
 
 // TODO: To change the following API calls into Moralis instead
 const api: AxiosInstance = axios.create({
