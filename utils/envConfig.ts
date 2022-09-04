912cd2293757b9e03ecdad2ece99baad5c8262e1
Eric Mong <m.changhsi@gmail.com>
2022-09-04 18:52:49 +0800

    Update environment variable config file

--- utils/envConfig.ts
+++ utils/envConfig.ts
@@ -1,14 +1,10 @@
 const envConfig = {
   MAINNET: process.env.NEXT_PUBLIC_MAINNET === 'true' ? true : false,
-  CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || '',
-  MINT_OVER: process.env.NEXT_PUBLIC_MINT_OVER === 'true' ? true : false,
-  CONTRACT_DEPLOYED:
-    process.env.NEXT_PUBLIC_CONTRACT_DEPLOYED === 'true' ? true : false,
-  INFURA_ID: process.env.NEXT_PUBLIC_INFURA_ID || '',
-  CONTRACT_NAME: process.env.NEXT_PUBLIC_CONTRACT_NAME || '',
   API_URL: process.env.NEXT_PUBLIC_API_URL || '',
   MORALIS_API_KEY: process.env.NEXT_PUBLIC_MORALIS_API_KEY || '',
-  WEB3_AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID || '',
+  WEB3AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || '',
+  ALCHEMY_ID: process.env.NEXT_PUBLIC_ALCHEMY_ID || '',
+  INFURA_ID: process.env.NEXT_PUBLIC_INFURA_ID || '',
 }
 
 export default envConfig
