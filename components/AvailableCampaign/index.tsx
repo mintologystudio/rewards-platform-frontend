130b9c79ff27411a70751be31ea021ec84f6a694
mlyk1234 <32647481+mlyk1234@users.noreply.github.com>
2022-11-08 03:42:15 +0800

    no message

--- components/AvailableCampaign/index.tsx
+++ components/AvailableCampaign/index.tsx
@@ -46,7 +46,7 @@ const AvailableCampaign = () => {
             : <></>
           }
 
-          { !campaign.isLoading && campaign.campaigns.length > 0 ? <></> : <></> }
+          { !campaign.isLoading && campaign.campaigns.length > 0 ? <MoreCampaignCard key={-1} /> : <></> }
 
             {/*campaignData.map((c: ICampaign) => (*/}
             {/*<CampaignCard key={c.campaignId}*/}
